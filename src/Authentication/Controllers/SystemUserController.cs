d96ecca39b782f3fcb8d6b583748575cb79dec0e
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-11-06 13:15:32 +0100

    SystemUser Controller CRUD and Model
    An empty CRUD scaffold to show the CRUD API, and the request and response for the SystemUser model

--- src/Authentication/Controllers/SystemUserController.cs
+++ src/Authentication/Controllers/SystemUserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Altinn.Platform.Authentication.Controllers
@@ -11,5 +12,62 @@ namespace Altinn.Platform.Authentication.Controllers
     [ApiController]
     public class SystemUserController : ControllerBase
     {
-    }
+        /// <summary>
+        /// Returns the list of SystemUsers this PartyID has registered
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("list/{partyId}")]
+        public async Task<ActionResult> GetListOfSystemUsersPartyHas()
+        {
+            await Task.Delay(50);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Return a single SystemUser by PartyId and SystemUserId
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("systemuser/{partyId}/{systemUserId}")]
+        public async Task<ActionResult> GetSingleSystemUserById()
+        {
+            await Task.Delay(50);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Set the Delete flag on the identified SystemUser
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("systemuser/{partyId}/{systemUserId}")]
+        public async Task<ActionResult> SetDeleteFlagOnSystemUser()
+        {
+            await Task.Delay(50);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Creates a new SystemUser
+        /// The unique Id for the systemuser is handled by the db.
+        /// But the calling client may send a guid for the request of creating a new system user
+        /// to ensure that there is no mismatch if the same partyId creates several new SystemUsers at the same time
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("systemuser/{partyId}/{createRequestId}")]
+        public async Task<ActionResult> CreateSystemUser()
+        {
+            await Task.Delay(50);
+            return Ok();
+        }
+
+        /// <summary>
+        /// Replaces the values for the existing system user with those from the update 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut("systemuser/{partyId}/{systemUserId}")]
+        public async Task<ActionResult> UpdateSystemUserById()
+        {
+            await Task.Delay(50);
+            return Ok();
+        }
+    }    
 }
---
+++ src/Authentication/Model/SystemUserResponse.cs
@@ -0,0 +1,71 @@
+﻿namespace Altinn.Platform.Authentication.Model
+{
+    /// <summary>
+    /// The model of the System User response given in the CRUD API in SystemUserController.cs
+    /// This model will be exchanged between this Authentication component, the PostGress db and the BFF for the Frontend.
+    /// The BFF will provide a tailored DTO to the Frontend.
+    /// </summary>
+    public class SystemUserResponse
+    {
+        /// <summary>
+        /// GUID created by the "real" Authentication Component
+        /// When the Frontend send a request for the 
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The Title and Description are strings set by the end-user in the Frontend.
+        /// </summary>
+        public string IntegrationTitle { get; set; }
+
+        /// <summary>
+        /// The user entered Description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// For off the shelf systems.
+        /// Should probably be human readable (instead of a GUID) but unique string without whitespace
+        /// The "real" Authentication Component should validate that the SystemName is unique
+        /// Retrieved from the SystemRegister, the full CRUD Api is in a different service
+        /// </summary>
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// The OwnedBy identifies the end-user Organisation, and is fetched from the login Context and
+        /// user party serivces
+        /// </summary>
+        public string OwnedByPartyId { get; set; }        
+
+        /// <summary>
+        /// For self-made systems, not delivered in the first Phase of the Project, and therefore not in the DTO
+        /// In these cases the SupplierName and SupplierOrgNo will be blank
+        /// </summary>
+        public string? ClientId { get; set; }
+
+        /// <summary>
+        /// Nice to have for debugging and logging.
+        /// </summary>
+        public string Created { get; set; }
+
+        /// <summary>
+        /// False by default, if a SystemUser is deleted in the API,
+        /// it is marked as IsDeleted ("tombstoned") rather than actually deleted
+        /// from the database. This is to avoid complications with cascade delete,
+        /// and the need to maintain consistent logging, and possible compliance rules.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// The name of the Supplier of the Product used in this Integration.
+        /// In later phases, it will be possible to use non-supplier based Products, in which case the ClientId property should be filled out.
+        /// </summary>
+        public string? SupplierName { get; set; }
+
+        /// <summary>
+        /// The organization number for the Supplier of the Product 
+        /// In later phases, it will be possible to use non-supplier based Products, in which case the ClientId property should be filled out.
+        /// </summary>
+        public string? SupplierOrgNo { get; set; }
+    }
+}
