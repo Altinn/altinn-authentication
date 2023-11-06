fecdf59dd2166ae2fd90a1feddfb806beec9feff
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-11-06 12:04:42 +0100

    Create SystemUserController.cs

---
+++ src/Authentication/Controllers/SystemUserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Platform.Authentication.Controllers
+{
+    /// <summary>
+    /// CRUD API for the System User 
+    /// </summary>
+    [Authorize]
+    [Route("authentication/api/v1/systemuser")]
+    [ApiController]
+    public class SystemUserController : ControllerBase
+    {
+    }
+}
