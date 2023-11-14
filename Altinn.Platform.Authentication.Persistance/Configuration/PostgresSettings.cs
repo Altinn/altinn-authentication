de92d413db59ce1b61514958bbd894c192bf680e
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-11-14 09:44:43 +0100

    Persistance folder structure

---
+++ Altinn.Platform.Authentication.Persistance/Configuration/PostgresSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Altinn.Platform.Authentication.Persistance.Configuration
+{
+    /// <summary>
+    /// Settings for the PostgreSQL db.
+    /// </summary>
+    public class PostgresSettings
+    {
+        /// <summary>
+        /// The connection string to the postgres db.
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        /// <summary>
+        /// PAssword for the app user for the postgres db.
+        /// </summary>
+        public string AuthorizationDbPwd { get; set; }
+    }
+}
