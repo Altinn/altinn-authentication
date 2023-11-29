14c6f130b35d68f0b2e63e42199334460f0cfd3f
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-11-29 13:22:25 +0100

    Create DependencyInjection.cs

---
+++ src/Persistance/Extensions/DependencyInjection.cs
@@ -0,0 +1,53 @@
+﻿using Altinn.Platform.Authentication.Persistance.Configuration;
+using Altinn.Platform.Authentication.RepositoryInterfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Npgsql;
+
+namespace Altinn.Platform.Authentication.Persistance.Extensions;
+
+/// <summary>
+/// Extension class used for injecting the repository into the Service Collection of the parent project.
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Used in the parent project to insert the Repositories and Postgres DB into the Dependency Injection Service Collection
+    /// </summary>
+    /// <param name="services">Parent project's Service Collection</param>
+    /// <returns></returns>
+    public static IServiceCollection AddPersistanceLayer(this IServiceCollection services)
+    {
+        AddPostgreSqlDatabase(services);
+        AddSystemUserRepository(services);        
+
+        return services;
+    }
+
+    private static void AddPostgreSqlDatabase(IServiceCollection services) 
+    {
+        services.AddOptions<PostgreSqlSettings>()
+                 .Validate(s => !string.IsNullOrEmpty(s.ConnectionString), "Missing Connection string")
+                 .Validate(s => !string.IsNullOrEmpty(s.AuthenticationDbPwd), "Missing db password");
+
+        services.TryAddSingleton((IServiceProvider sp) =>
+        {
+            var settings = sp.GetRequiredService<IOptions<PostgreSqlSettings>>().Value;
+            var connectionString = string.Format(
+                settings.ConnectionString,
+                settings.AuthenticationDbPwd);
+
+            var builder = new NpgsqlDataSourceBuilder(connectionString);
+            builder.UseLoggerFactory(sp.GetRequiredService<ILoggerFactory>());
+        });
+
+        services.TryAddTransient((IServiceProvider sp) => sp.GetRequiredService<NpgsqlDataSource>().CreateConnection());
+    }
+
+    private static void AddSystemUserRepository(IServiceCollection services) 
+    {
+        services.TryAddTransient<ISystemUserRespository, SystemUserRespository>();
+    }
+}
