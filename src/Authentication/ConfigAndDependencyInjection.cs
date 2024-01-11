3f4ca2f1e9f3a7c3f27b277671750444e105f72b
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-01-11 13:14:57 +0100

    sonarcloud fixes 4

---
+++ src/Authentication/ConfigAndDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Altinn.Platform.Authentication
+{
+    /// <summary>
+    /// Used to make the top level file Program.cs more readable
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class ConfigAndDependencyInjection
+    {
+    }
+}
