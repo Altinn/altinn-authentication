2988b55dd21844f25daa9ddb6e7596dae62afa4e
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-05-22 09:25:14 +0200

    Sr fix broken deploy 2 (#542)
    * test av migration script og idporten åpning til endepunkt
    
    * idporten hello
    yuniqle path fix
    dockerfile improvements
    
    * changed user name for postgres
    
    * fix

--- src/Persistance/Migration/v0.00/05-setup-schema-grants.sql
+++ src/Persistance/Migration/v0.00/05-setup-schema-grants.sql
@@ -1,3 +1,3 @@
-﻿GRANT USAGE ON SCHEMA altinn_authentication_integration TO platform_authentication;
-GRANT SELECT,INSERT,UPDATE,REFERENCES,DELETE,TRUNCATE,TRIGGER ON ALL TABLES IN SCHEMA altinn_authentication_integration TO platform_authentication;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA altinn_authentication_integration TO platform_authentication;
+﻿GRANT USAGE ON SCHEMA altinn_authentication_integration TO auth_authentication;
+GRANT SELECT,INSERT,UPDATE,REFERENCES,DELETE,TRUNCATE,TRIGGER ON ALL TABLES IN SCHEMA altinn_authentication_integration TO auth_authentication;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA altinn_authentication_integration TO auth_authentication;
