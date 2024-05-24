191d19a9701dace3c7e613a7287a48d4997430ed
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-05-24 15:04:29 +0200

    reenable authorize

--- src/Authentication/Controllers/SystemUserController.cs
+++ src/Authentication/Controllers/SystemUserController.cs
@@ -37,7 +37,7 @@ namespace Altinn.Platform.Authentication.Controllers
         /// Returns the list of SystemUsers this PartyID has registered
         /// </summary>
         /// <returns></returns>
-        //[Authorize]
+        [Authorize]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{partyId}")]
@@ -138,7 +138,7 @@ namespace Altinn.Platform.Authentication.Controllers
         /// to ensure that there is no mismatch if the same partyId creates several new SystemUsers at the same time
         /// </summary>
         /// <returns></returns>        
-        //[Authorize]
+        [Authorize]
         [Produces("application/json")]
         [ProducesResponseType(typeof(SystemUser), StatusCodes.Status200OK)]        
         [ProducesResponseType(StatusCodes.Status404NotFound)]        
