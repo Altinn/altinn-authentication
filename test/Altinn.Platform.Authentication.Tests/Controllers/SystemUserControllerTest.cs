8635d33015bf0e9e4911e713c21f2d48fc229d00
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-06-17 12:07:04 +0200

    also fixed a test

--- test/Altinn.Platform.Authentication.Tests/Controllers/SystemUserControllerTest.cs
+++ test/Altinn.Platform.Authentication.Tests/Controllers/SystemUserControllerTest.cs
@@ -289,7 +289,7 @@ namespace Altinn.Platform.Authentication.Tests.Controllers
                 ProductName = "ProductNameValue"
             };
 
-            HttpRequestMessage request2 = new(HttpMethod.Post, $"/authentication/api/v1/systemuser");
+            HttpRequestMessage request2 = new(HttpMethod.Post, $"/authentication/api/v1/systemuser/{partyId}");
             request2.Content = JsonContent.Create<SystemUserRequestDto>(newSystemUser, new MediaTypeHeaderValue("application/json"));
             HttpResponseMessage response2 = await client.SendAsync(request2, HttpCompletionOption.ResponseContentRead);
                          
