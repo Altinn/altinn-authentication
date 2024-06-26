3196ee5937f3ffe031178543e5c43586f8d760d8
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-06-26 14:03:34 +0200

    removed noise in script

--- src/Persistance/Migration/v0.00/04-setup-user-table.sql
+++ src/Persistance/Migration/v0.00/04-setup-user-table.sql
@@ -5,10 +5,10 @@
 CREATE TABLE IF NOT EXISTS business_application.system_user_profile
 (
     system_user_profile_id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
-    integration_title character varying(255) COLLATE pg_catalog."default" NOT NULL,    
+    integration_title character varying(255) NOT NULL,    
     system_internal_id uuid NOT NULL,
-    reportee_party_id character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    reportee_org_no character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    reportee_party_id character varying(255) NOT NULL,
+    reportee_org_no character varying(255) NOT NULL,
     created timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_changed timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     is_deleted boolean DEFAULT false,
