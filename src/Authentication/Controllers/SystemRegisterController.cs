f7b77a4c9bc97bbf541df0854a4581b6a3010b89
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2024-08-21 14:14:58 +0200

    removed the comment on authorize

--- src/Authentication/Controllers/SystemRegisterController.cs
+++ src/Authentication/Controllers/SystemRegisterController.cs
@@ -14,7 +14,7 @@ namespace Altinn.Authentication.Controllers;
 /// <summary>
 /// CRUD API for SystemRegister
 /// </summary>
-///[Authorize]
+[Authorize]
 [Route("authentication/api/v1/systemregister")]
 [ApiController]
 public class SystemRegisterController : ControllerBase
@@ -85,7 +85,7 @@ public class SystemRegisterController : ControllerBase
     /// <param name="cancellationToken">The Cancellationtoken</param>
     /// <returns></returns>
     [HttpPost("system")]    
-    ///[Authorize(Policy = AuthzConstants.POLICY_SCOPE_SYSTEMREGISTER_WRITE)]
+    [Authorize(Policy = AuthzConstants.POLICY_SCOPE_SYSTEMREGISTER_WRITE)]
     public async Task<ActionResult<Guid>> CreateRegisteredSystem([FromBody] RegisterSystemRequest registerNewSystem, CancellationToken cancellationToken = default)
     {
         try
