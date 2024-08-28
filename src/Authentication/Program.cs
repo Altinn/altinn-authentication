11781c46b03061861edfd8025515a2aa4937a981
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-08-28 12:39:55 +0200

    reporteeorgno -> party

--- src/Authentication/Program.cs
+++ src/Authentication/Program.cs
@@ -381,31 +381,6 @@ void ConfigureServices(IServiceCollection services, IConfiguration config)
     {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "Altinn Platform Authentication", Version = "v1" });
 
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-        {
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey,
-            Scheme = "Bearer",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
-        });
-
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement 
-        {
-            {
-                new OpenApiSecurityScheme 
-                {
-                    Reference = new OpenApiReference 
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                Array.Empty<string>()
-            }
-        });
-
         try
         {
             string filePath = GetXmlCommentsPathForControllers();
