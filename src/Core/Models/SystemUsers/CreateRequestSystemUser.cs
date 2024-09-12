228f337ebdce7df7ba02f69ffc5d312e21b2252a
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-12 16:00:40 +0200

    moved doc summary on the DTOs

--- src/Core/Models/SystemUsers/CreateRequestSystemUser.cs
+++ src/Core/Models/SystemUsers/CreateRequestSystemUser.cs
@@ -3,17 +3,32 @@
 /// <summary>
 /// DTO to create a new Request for a SystemUser
 /// </summary>
-/// <param name="ExternalRef">Either just the Orgno for the customer, or a TenantId or other form of disambiguation Id the Vendor needs.</param>
-/// <param name="SystemId">The Id for the Registered System that this Request will be based on.</param>
-/// <param name="PartyOrgNo">The organisation number for the SystemUser's Party ( the customer that delegates rights to the systemuser) </param>
-/// <param name="Rights">The set of Rights requested for this system user. Must be equal to or less than the set defined in the Registered System</param>
-/// <param name="RedirectURL">Optional redirect URL to navigate to after the customer has accepted/denied the Request</param>
+
 public class CreateRequestSystemUser()
-{ 
+{
+    /// <summary>
+    /// Either just the Orgno for the customer, or a TenantId or other form of disambiguation Id the Vendor needs.
+    /// </summary>
     public string ExternalRef { get; set; }
+
+    /// <summary>
+    /// The Id for the Registered System that this Request will be based on.
+    /// </summary>
     public string SystemId { get; set; }
+
+    /// <summary>
+    /// The organisation number for the SystemUser's Party ( the customer that delegates rights to the systemuser) 
+    /// </summary>
     public string PartyOrgNo { get; set; }
+
+    /// <summary>
+    /// The set of Rights requested for this system user. Must be equal to or less than the set defined in the Registered System
+    /// </summary>
     public List<Right> Rights { get; set; }
-    public string? RedirectURL { get; set; } 
+
+    /// <summary>
+    /// Optional redirect URL to navigate to after the customer has accepted/denied the Request
+    /// </summary>
+    public string? RedirectURL { get; set; }
 }
 
