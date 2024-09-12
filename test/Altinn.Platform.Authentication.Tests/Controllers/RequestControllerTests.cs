c15eeb1224c2e0615bf4379abeb95f2f25ee8784
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-12 14:33:43 +0200

    simple test

---
+++ test/Altinn.Platform.Authentication.Tests/Controllers/RequestControllerTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Altinn.Common.AccessToken.Services;
+using Altinn.Platform.Authentication.Clients.Interfaces;
+using Altinn.Platform.Authentication.Configuration;
+using Altinn.Platform.Authentication.Core.Models;
+using Altinn.Platform.Authentication.Core.Models.SystemUsers;
+using Altinn.Platform.Authentication.Core.SystemRegister.Models;
+using Altinn.Platform.Authentication.Services;
+using Altinn.Platform.Authentication.Services.Interfaces;
+using Altinn.Platform.Authentication.Tests.Fakes;
+using Altinn.Platform.Authentication.Tests.Mocks;
+using Altinn.Platform.Authentication.Tests.RepositoryDataAccess;
+using Altinn.Platform.Authentication.Tests.Utils;
+using Altinn.Platform.Authentication.Core.Extensions;
+using AltinnCore.Authentication.JwtCookie;
+using App.IntegrationTests.Utils;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace Altinn.Platform.Authentication.Tests.Controllers
+{
+    public class RequestControllerTests(DbFixture dbFixture, WebApplicationFixture webApplicationFixture)
+        : WebApplicationTests(dbFixture, webApplicationFixture)
+    {
+        private static readonly JsonSerializerOptions _options = new(JsonSerializerDefaults.Web);
+        
+        private readonly Mock<IUserProfileService> _userProfileService = new();
+        private readonly Mock<ISblCookieDecryptionService> _sblCookieDecryptionService = new();
+
+        private readonly Mock<TimeProvider> timeProviderMock = new();
+        private readonly Mock<IGuidService> guidService = new();
+        private readonly Mock<IEventsQueueClient> _eventQueue = new();
+
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+            bool enableOidc = false;
+            bool forceOidc = false;
+            string defaultOidc = "altinn";
+
+            string configPath = GetConfigPath();
+
+            WebHostBuilder builder = new();
+
+            builder.ConfigureAppConfiguration((context, conf) =>
+            {
+                conf.AddJsonFile(configPath);
+            });
+
+            var configuration = new ConfigurationBuilder()
+              .AddJsonFile(configPath)
+              .Build();
+
+            configuration.GetSection("GeneralSettings:EnableOidc").Value = enableOidc.ToString();
+            configuration.GetSection("GeneralSettings:ForceOidc").Value = forceOidc.ToString();
+            configuration.GetSection("GeneralSettings:DefaultOidcProvider").Value = defaultOidc;
+
+            IConfigurationSection generalSettingSection = configuration.GetSection("GeneralSettings");
+            
+            services.Configure<GeneralSettings>(generalSettingSection);
+            services.AddSingleton<IOrganisationsService, OrganisationsServiceMock>();
+            services.AddSingleton<ISigningKeysRetriever, SigningKeysRetrieverStub>();
+            services.AddSingleton<IJwtSigningCertificateProvider, JwtSigningCertificateProviderStub>();
+            services.AddSingleton<IPostConfigureOptions<JwtCookieOptions>, JwtCookiePostConfigureOptionsStub>();
+            services.AddSingleton<IPublicSigningKeyProvider, SigningKeyResolverStub>();
+            services.AddSingleton<IEnterpriseUserAuthenticationService, EnterpriseUserAuthenticationServiceMock>();
+            services.AddSingleton<IOidcProvider, OidcProviderServiceMock>();
+            services.AddSingleton(_eventQueue.Object);
+            services.AddSingleton(timeProviderMock.Object);
+            services.AddSingleton(guidService.Object);
+            services.AddSingleton<IUserProfileService>(_userProfileService.Object);
+            services.AddSingleton<ISblCookieDecryptionService>(_sblCookieDecryptionService.Object);
+            services.AddSingleton<ISystemUserService, SystemUserServiceMock>();    
+            services.AddSingleton<ISystemRegisterService, SystemRegisterService>();
+            services.AddSingleton<IRequestSystemUser, RequestSystemUserService>();
+            SetupDateTimeMock();
+            SetupGuidMock();
+        }
+
+        [Fact]
+        public async Task Request_Create_Success()
+        {
+            HttpClient client = CreateClient();
+            string[] prefixes = { "altinn", "digdir" };
+            string token = PrincipalUtil.GetOrgToken("digdir", "991825827", "altinn:authentication/systemregister.admin", prefixes);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            string endpoint = $"/authentication/api/v1/request";
+
+            // Arrange
+            CreateRequestSystemUser req = new(
+                Guid.NewGuid(),
+                "external",
+                "systemId",
+                "1234567",
+                []);
+             
+            HttpResponseMessage message = await client.PostAsync(token, endpoint, JsonContent.Create(req));
+            Assert.Equal(System.Net.HttpStatusCode.OK, message.StatusCode);       
+            CreateRequestSystemUserResponse res = await message.Content.ReadFromJsonAsync<CreateRequestSystemUserResponse>();
+            Assert.Equal(req.Id, res.Id);
+        }
+
+        private void SetupDateTimeMock()
+        {
+            timeProviderMock.Setup(x => x.GetUtcNow()).Returns(new DateTimeOffset(2018, 05, 15, 02, 05, 00, TimeSpan.Zero));
+        }
+
+        private void SetupGuidMock()
+        {
+            guidService.Setup(q => q.NewGuid()).Returns("eaec330c-1e2d-4acb-8975-5f3eba12b2fb");
+        }
+
+        private static string GetConfigPath()
+        {
+            string unitTestFolder = Path.GetDirectoryName(new Uri(typeof(AuthenticationControllerTests).Assembly.Location).LocalPath);
+            return Path.Combine(unitTestFolder, $"../../../appsettings.json");
+        }
+    }
+}
