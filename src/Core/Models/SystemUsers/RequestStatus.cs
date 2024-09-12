08bbf148f9c83158d760b3c641ae81bcbf82fec8
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-12 14:08:12 +0200

    enum comment

--- src/Core/Models/SystemUsers/RequestStatus.cs
+++ src/Core/Models/SystemUsers/RequestStatus.cs
@@ -1,5 +1,9 @@
 ﻿namespace Altinn.Platform.Authentication.Core.Models.SystemUsers;
 
+/// <summary>
+/// The status for the Request SystemUser
+/// When a Request is archived/softdeleted, the last current status is not changed
+/// </summary>
 public enum RequestStatus
 {
     New,
