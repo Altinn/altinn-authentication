20a473811a31a35511501a473db09ca23fea7fca
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-13 14:49:05 +0200

    Feature/l2 get request systemuser (#707)
    * GET by guid
    GET by extref + systemid
    
    * Tests for Get
    
    * comments
    
    * comments about validation's needed

--- src/Authentication/Services/RequestSystemUserService.cs
+++ src/Authentication/Services/RequestSystemUserService.cs
@@ -13,6 +13,11 @@ public class RequestSystemUserService : IRequestSystemUser
     {
         await Task.Delay(65);
 
+        // Validate the PartyOrgNo for the Customer
+        // Valiate that the Vendor's OrgNo owns the chosen SystemId ( retrieved from token)
+        // Validate that the combination of System and External ref does not currently exist (not soft-deleted)
+        // Validate that the RedirectUrl chosen is the same as one of the RedirectUrl's listed for the Registered System
+        // Set an empty ExternalRef to be equal to the PartyOrgNo
         return new CreateRequestSystemUserResponse()
         {
             Id = Guid.NewGuid(),
@@ -22,6 +27,42 @@ public class RequestSystemUserService : IRequestSystemUser
             Rights = createRequest.Rights,
             Status = RequestStatus.New.ToString(),
             RedirectURL = createRequest.RedirectURL,
+            SystemUserId = Guid.NewGuid().ToString()
+        };
+    }
+
+    /// <inheritdoc/>
+    public async Task<CreateRequestSystemUserResponse> GetRequestByExternalRef(string systemId, string externalRef1)
+    {
+        await Task.Delay(65);
+
+        return new CreateRequestSystemUserResponse()
+        {
+            Id = Guid.NewGuid(),
+            ExternalRef = externalRef1,
+            SystemId = systemId,
+            PartyOrgNo = "1234567",
+            Rights = [],
+            Status = RequestStatus.New.ToString(),
+            RedirectURL = null,
+            SystemUserId = Guid.NewGuid().ToString()
+        };
+    }
+
+    /// <inheritdoc/>
+    public async Task<CreateRequestSystemUserResponse> GetRequestByGuid(Guid requestId)
+    {
+        await Task.Delay(65);
+
+        return new CreateRequestSystemUserResponse()
+        {
+            Id = requestId,
+            ExternalRef = "1234567",
+            SystemId = "1234567_cool_system",
+            PartyOrgNo = "1234567",
+            Rights = [],
+            Status = RequestStatus.New.ToString(),
+            RedirectURL = null,
             SystemUserId = Guid.NewGuid().ToString()
         };
     }
--- src/Core/Models/SystemUsers/CreateRequestSystemUser.cs
+++ src/Core/Models/SystemUsers/CreateRequestSystemUser.cs
@@ -1,29 +1,40 @@
-﻿namespace Altinn.Platform.Authentication.Core.Models.SystemUsers;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Altinn.Platform.Authentication.Core.Models.SystemUsers;
 
 /// <summary>
 /// DTO to create a new Request for a SystemUser
 /// </summary>
-
 public class CreateRequestSystemUser()
 {
     /// <summary>
-    /// Either just the Orgno for the customer, or a TenantId or other form of disambiguation Id the Vendor needs.
+    /// Either just the same as the PartyOrgNo for the customer, 
+    /// or a TenantId or other form of disambiguation Id,
+    /// the Vendor has control over themselves to ensure uniqueness.
+    /// A null ExternalRef will default to the PartyOrgNo.
     /// </summary>
-    public string ExternalRef { get; set; }
+    public string? ExternalRef { get; set; }
 
     /// <summary>
-    /// The Id for the Registered System that this Request will be based on.
+    /// The Id for the Registered System that this Request will be based on. 
+    /// Must be owned by the Vendor that creates the Request.
     /// </summary>
+    [Required]
     public string SystemId { get; set; }
 
     /// <summary>
-    /// The organisation number for the SystemUser's Party ( the customer that delegates rights to the systemuser) 
+    /// The organisation number for the SystemUser's Party 
+    /// ( the customer that delegates rights to the systemuser) 
     /// </summary>
+    [Required]
     public string PartyOrgNo { get; set; }
 
     /// <summary>
-    /// The set of Rights requested for this system user. Must be equal to or less than the set defined in the Registered System
+    /// The set of Rights requested for this system user. 
+    /// Must be equal to or less than the set defined in the Registered System.
+    /// An empty list will throw an error.
     /// </summary>
+    [Required]
     public List<Right> Rights { get; set; }
 
     /// <summary>
@@ -31,4 +42,3 @@ public class CreateRequestSystemUser()
     /// </summary>
     public string? RedirectURL { get; set; }
 }
-
