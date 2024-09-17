cce286d13dd97b8e48f75f99ec93ab824d045965
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2024-09-18 00:16:16 +0200

    Added updateclient validations and method to update client information on update register

---
+++ src/Core/Models/MaskinPortenClientInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace Altinn.Platform.Authentication.Core.Models
+{
+    /// <summary>
+    /// DTO for the maskinporten client information for the system
+    /// </summary>
+    public class MaskinPortenClientInfo
+    {
+
+        /// <summary>
+        /// Maskinporten client id for the system
+        /// </summary>                
+        public required string ClientId { get; set; }
+
+        /// <summary>
+        /// internal id for the system
+        /// </summary>                
+        public required Guid SystemInternalId { get; set; }
+
+        /// <summary>
+        /// true if the clientid is not in use
+        /// </summary>
+        public bool IsDeleted { get; set; }
+    }
+}
