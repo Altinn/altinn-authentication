1820ea43329e25e0164f134bc5a3a2ee6b6a34a7
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-10-01 15:29:01 +0200

    pagination classes needed

---
+++ src/Authentication/Model/Base64UrlEncoder.cs
@@ -0,0 +1,233 @@
+﻿#nullable enable
+
+using System;
+using System.Buffers;
+using System.Buffers.Text;
+
+namespace Altinn.Platform.Authentication.Model;
+
+/// <summary>
+/// Utility class for base 64 url encoding and decoding using the base64 web safe alphabet.
+/// </summary>
+internal static class Base64UrlEncoder
+{
+    private static readonly SearchValues<char> ReplacedValues = SearchValues.Create("-_");
+    private static readonly SearchValues<byte> ReplacedValuesUtf8 = SearchValues.Create([(byte)'-', (byte)'_']);
+
+    /// <summary>
+    /// Returns the maximum length (in bytes) of the result if you were to decode base 64 encoded text within a char span of size "length".
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown when the specified <paramref name="length"/> is less than 0.
+    /// </exception>
+    public static int GetMaxDecodedLength(int length)
+    {
+        return Base64.GetMaxDecodedFromUtf8Length(length + 2);
+    }
+
+    /// <summary>
+    /// Returns the maximum length (in chars) of the result if you were to encode binary data within a byte span of size "length".
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown when the specified <paramref name="length"/> is less than 0 or larger than 1610612733 (since encode inflates the data by 4/3).
+    /// </exception>
+    public static int GetMaxEncodedLength(int length)
+    {
+        return Base64.GetMaxEncodedToUtf8Length(length);
+    }
+
+    /// <summary>
+    /// Encode binary data within a byte span into base64 encoded text.
+    /// </summary>
+    /// <param name="data">The data to encode</param>
+    /// <returns>The resulting base64 string</returns>
+    public static string Encode(ReadOnlySpan<byte> data)
+    {
+        var buff = ArrayPool<char>.Shared.Rent(GetMaxEncodedLength(data.Length));
+        try
+        {
+            Convert.TryToBase64Chars(data, buff, out var charsWritten);
+            var written = buff.AsSpan(0, charsWritten).TrimEnd('=');
+            written.Replace('+', '-');
+            written.Replace('/', '_');
+            return new string(written);
+        }
+        finally
+        {
+            ArrayPool<char>.Shared.Return(buff);
+        }
+    }
+
+    /// <summary>
+    /// Try to encode binary data within a span into base64 encoded utf8 bytes.
+    /// </summary>
+    /// <param name="data">The data to encode</param>
+    /// <param name="destination">The destination to write the utf8 bytes to</param>
+    /// <param name="bytesWritten">The number of bytes written</param>
+    /// <returns><see langword="true"/> if encoding succeeded, otherwise <see langword="false"/></returns>
+    public static bool TryEncode(ReadOnlySpan<byte> data, Span<byte> destination, out int bytesWritten)
+    {
+        var result = Base64.EncodeToUtf8(data, destination, out var consumed, out var written, isFinalBlock: true);
+        if (result != OperationStatus.Done)
+        {
+            bytesWritten = 0;
+            return false;
+        }
+
+        if (consumed != data.Length)
+        {
+            bytesWritten = 0;
+            return false;
+        }
+
+        var writtenUtf8 = destination[..written].TrimEnd((byte)'=');
+        writtenUtf8.Replace((byte)'+', (byte)'-');
+        writtenUtf8.Replace((byte)'/', (byte)'_');
+        bytesWritten = writtenUtf8.Length;
+        return true;
+    }
+
+    /// <summary>
+    /// Try to decode base 64 encoded text within a char span into a byte span.
+    /// </summary>
+    /// <param name="encoded">The encoded text</param>
+    /// <param name="data">The span of data to write into</param>
+    /// <param name="bytesWritten">The number of bytes written</param>
+    /// <returns><see langword="true"/> if decoding succeeded, otherwise <see langword="false"/></returns>
+    public static bool TryDecode(ReadOnlySpan<char> encoded, Span<byte> data, out int bytesWritten)
+    {
+        if (encoded.ContainsAny(ReplacedValues) || NeedsPadding(encoded))
+        {
+            return TryReplaceAndDecode(encoded, data, out bytesWritten);
+        }
+        else
+        {
+            return TryDecodeInner(encoded, data, out bytesWritten);
+        }
+    }
+
+    /// <summary>
+    /// Try to decode base 64 encoded text within a utf8 byte span into a byte span.
+    /// </summary>
+    /// <param name="encoded">The encoded text as utf8 bytes</param>
+    /// <param name="data">The span of data to write into</param>
+    /// <param name="bytesWritten">The number of bytes written</param>
+    /// <returns><see langword="true"/> if decoding succeeded, otherwise <see langword="false"/></returns>
+    public static bool TryDecode(ReadOnlySpan<byte> encoded, Span<byte> data, out int bytesWritten)
+    {
+        if (encoded.ContainsAny(ReplacedValuesUtf8) || NeedsPadding(encoded))
+        {
+            return TryReplaceAndDecode(encoded, data, out bytesWritten);
+        }
+        else
+        {
+            return TryDecodeInner(encoded, data, out bytesWritten);
+        }
+    }
+
+    private static bool TryReplaceAndDecode(ReadOnlySpan<char> encoded, Span<byte> data, out int bytesWritten)
+    {
+        var length = encoded.Length;
+        var chars = ArrayPool<char>.Shared.Rent(length + 2);
+        try
+        {
+            encoded.CopyTo(chars.AsSpan());
+            chars.AsSpan(0, length).Replace('-', '+');
+            chars.AsSpan(0, length).Replace('_', '/');
+
+            encoded = PadIfNeeded(chars, length);
+
+            return TryDecodeInner(encoded, data, out bytesWritten);
+        }
+        finally
+        {
+            ArrayPool<char>.Shared.Return(chars);
+        }
+    }
+
+    private static bool TryReplaceAndDecode(ReadOnlySpan<byte> encoded, Span<byte> data, out int bytesWritten)
+    {
+        var length = encoded.Length;
+        var bytes = ArrayPool<byte>.Shared.Rent(length + 2);
+        try
+        {
+            encoded.CopyTo(bytes.AsSpan());
+            bytes.AsSpan(0, length).Replace((byte)'-', (byte)'+');
+            bytes.AsSpan(0, length).Replace((byte)'_', (byte)'/');
+
+            encoded = PadIfNeeded(bytes, length);
+
+            return TryDecodeInner(encoded, data, out bytesWritten);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(bytes);
+        }
+    }
+
+    private static bool TryDecodeInner(ReadOnlySpan<char> encoded, Span<byte> data, out int bytesWritten)
+    {
+        return Convert.TryFromBase64Chars(encoded, data, out bytesWritten);
+    }
+
+    private static bool TryDecodeInner(ReadOnlySpan<byte> encoded, Span<byte> data, out int bytesWritten)
+    {
+        var result = Base64.DecodeFromUtf8(encoded, data, out var consumed, out var written, isFinalBlock: true);
+        if (result != OperationStatus.Done)
+        {
+            bytesWritten = 0;
+            return false;
+        }
+
+        if (consumed != encoded.Length)
+        {
+            bytesWritten = 0;
+            return false;
+        }
+        
+        bytesWritten = written;
+        return true;
+    }
+
+    private static bool NeedsPadding(ReadOnlySpan<char> encoded)
+    {
+        return encoded.Length % 4 != 0;
+    }
+
+    private static bool NeedsPadding(ReadOnlySpan<byte> encoded)
+    {
+        return encoded.Length % 4 != 0;
+    }
+
+    private static Span<char> PadIfNeeded(Span<char> chars, int length)
+    {
+        switch (length % 4)
+        {
+            case 2:
+                chars[length] = '=';
+                chars[length + 1] = '=';
+                return chars[..(length + 2)];
+            case 3:
+                chars[length] = '=';
+                return chars[..(length + 1)];
+            default:
+                return chars[..length];
+        }
+    }
+
+    private static Span<byte> PadIfNeeded(Span<byte> chars, int length)
+    {
+        switch (length % 4)
+        {
+            case 2:
+                chars[length] = (byte)'=';
+                chars[length + 1] = (byte)'=';
+                return chars[..(length + 2)];
+            case 3:
+                chars[length] = (byte)'=';
+                return chars[..(length + 1)];
+            default:
+                return chars[..length];
+        }
+    }
+}
---
+++ src/Authentication/Model/Opaque.cs
@@ -0,0 +1,329 @@
+﻿#nullable enable
+
+using System;
+using System.Buffers;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CommunityToolkit.Diagnostics;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Annotations;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Altinn.Platform.Authentication.Model;
+
+/// <summary>
+/// An opaque value is a value that can be transmitted to another party
+/// without divulging any type information or expectations about the value.
+/// </summary>
+public static partial class Opaque
+{
+    /// <summary>
+    /// Create a new opaque value.
+    /// </summary>
+    /// <typeparam name="T">The type of the inner value</typeparam>
+    /// <param name="value">The inner value</param>
+    /// <returns>A new opaque value.</returns>
+    public static Opaque<T> Create<T>(T value) 
+        => new(value);
+
+    /// <summary>
+    /// Returns the etag type for a <see cref="Opaque{T}"/> type.
+    /// </summary>
+    /// <param name="opaqueType">The <see cref="Opaque{T}"/> type.</param>
+    /// <returns>
+    /// The type argument of the <paramref name="opaqueType"/> parameter, 
+    /// if the <paramref name="opaqueType"/> is a closed generic request 
+    /// opaque type, otherwise <see langword="null"/>.
+    /// </returns>
+    public static Type? GetUnderlyingType(Type opaqueType)
+    {
+        Guard.IsNotNull(opaqueType);
+
+        if (opaqueType.IsGenericType && !opaqueType.IsGenericTypeDefinition)
+        {
+            var genericType = opaqueType.GetGenericTypeDefinition();
+            if (ReferenceEquals(genericType, typeof(Opaque<>)))
+            {
+                return opaqueType.GetGenericArguments()[0];
+            }
+        }
+
+        return null;
+    }
+}
+
+/// <summary>
+///     <para>
+///     An opaque value is a value that can be transmitted to another party
+///     without divulging any type information or expectations about the value.
+/// </para>
+/// <para>
+///     Opaque values are typically usefull in APIs where the server wants to
+///     be able to return a value that the client later needs to send back to
+///     the server, but where the server does not want to expose the type of
+///     the value to the client. For instance, in a pagination scenario, the
+///     server can use an opaque int to do pagination by page number, while
+///     allowing itself to later change the implementation to use a cursor
+///     instead of a page number without breaking the API.
+/// </para>
+/// </summary>
+/// <typeparam name="T">The type of the inner value</typeparam>
+/// <param name="value">The inner value</param>
+[SwaggerSchemaFilter(typeof(Opaque.OpaqueSchemaFilter))]
+[JsonConverter(typeof(Opaque.OpaqueJsonConverter))]
+public class Opaque<T>(T value)
+    : IParsable<Opaque<T>>
+    , ISpanParsable<Opaque<T>>
+    , IUtf8SpanParsable<Opaque<T>>
+{
+    /// <summary>
+    /// Gets the inner value.
+    /// </summary>
+    public T Value => value;
+
+    /// <inheritdoc/>
+    public override string ToString()
+        => Base64UrlEncoder.Encode(JsonSerializer.SerializeToUtf8Bytes(value));
+
+    /// <inheritdoc/>
+    public static Opaque<T> Parse(string s, IFormatProvider? provider)
+        => Parse(s.AsSpan(), provider);
+
+    /// <inheritdoc/>
+    public static Opaque<T> Parse(ReadOnlySpan<char> s, IFormatProvider? provider)
+    {
+        if (!Opaque<T>.TryParse(s, provider, out var result))
+        {
+            throw new FormatException($"Failed to parse opaque {typeof(T).FullName}");
+        }
+
+        return result;
+    }
+
+    /// <inheritdoc/>
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Opaque<T> result)
+    {
+        if (s is null)
+        {
+            result = null;
+            return false;
+        }
+
+        return TryParse(s.AsSpan(), provider, out result);
+    }
+
+    /// <inheritdoc/>
+    public static Opaque<T> Parse(ReadOnlySpan<byte> s, IFormatProvider? provider)
+    {
+        if (!Opaque<T>.TryParse(s, provider, out var result))
+        {
+            throw new FormatException($"Failed to parse opaque {typeof(T).FullName}");
+        }
+
+        return result;
+    }
+
+    /// <inheritdoc/>
+    public static bool TryParse(ReadOnlySpan<char> s, IFormatProvider? provider, [MaybeNullWhen(false)] out Opaque<T> result)
+    {
+        byte[] buff = null!;
+        var binaryLength = Base64UrlEncoder.GetMaxDecodedLength(s.Length);
+        try 
+        {
+            buff = ArrayPool<byte>.Shared.Rent(binaryLength);
+            if (!Base64UrlEncoder.TryDecode(s, buff, out var written))
+            {
+                result = null;
+                return false;
+            }
+
+            var bytes = buff.AsSpan(0, written);
+            var inner = JsonSerializer.Deserialize<T>(bytes);
+            if (inner is null)
+            {
+                result = null;
+                return false;
+            }
+
+            result = new Opaque<T>(inner);
+            return true;
+        }
+        catch (JsonException)
+        {
+            result = null;
+            return false;
+        }
+        finally
+        {
+            if (buff is not null)
+            {
+                ArrayPool<byte>.Shared.Return(buff);
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public static bool TryParse(ReadOnlySpan<byte> s, IFormatProvider? provider, [MaybeNullWhen(false)] out Opaque<T> result)
+    {
+        byte[] buff = null!;
+        var binaryLength = Base64UrlEncoder.GetMaxDecodedLength(s.Length);
+        try
+        {
+            buff = ArrayPool<byte>.Shared.Rent(binaryLength);
+            if (!Base64UrlEncoder.TryDecode(s, buff, out var written))
+            {
+                result = null;
+                return false;
+            }
+
+            var bytes = buff.AsSpan(0, written);
+            var inner = JsonSerializer.Deserialize<T>(bytes);
+            if (inner is null)
+            {
+                result = null;
+                return false;
+            }
+
+            result = new Opaque<T>(inner);
+            return true;
+        }
+        catch (JsonException)
+        {
+            result = null;
+            return false;
+        }
+        finally
+        {
+            if (buff is not null)
+            {
+                ArrayPool<byte>.Shared.Return(buff);
+            }
+        }
+    }
+}
+
+[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1601:Partial elements should be documented", Justification = "Documented earler in the file")]
+public static partial class Opaque
+{
+    /// <summary>
+    /// Schema filter for opaque types
+    /// </summary>
+    internal class OpaqueSchemaFilter
+        : ISchemaFilter
+    {
+        /// <inheritdoc/>
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            schema.ExternalDocs = null;
+            schema.Type = "string";
+        }
+    }
+
+    /// <summary>
+    /// <see cref="JsonConverterFactory"/> for <see cref="Opaque{T}"/>
+    /// </summary>
+    internal class OpaqueJsonConverter : JsonConverterFactory
+    {
+        private ImmutableDictionary<Type, JsonConverter> _converters
+            = ImmutableDictionary<Type, JsonConverter>.Empty;
+
+        /// <inheritdoc/>
+        public override bool CanConvert(Type typeToConvert)
+            => Opaque.GetUnderlyingType(typeToConvert) is not null;
+
+        /// <inheritdoc/>
+        public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (Opaque.GetUnderlyingType(typeToConvert) is not { } innerType)
+            {
+                return null;
+            }
+
+            return ImmutableInterlocked.GetOrAdd(ref _converters, innerType, CreateConverterInner);
+        }
+
+        private static JsonConverter CreateConverterInner(Type opaqueInnerType)
+        {
+            var converterType = typeof(OpaqueJsonConverter<>).MakeGenericType(opaqueInnerType);
+            var converter = converterType
+                .GetProperty(nameof(OpaqueJsonConverter<object>.Instance), BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public)!
+                .GetValue(null)!;
+            return (JsonConverter)converter;
+        }
+    }
+
+    private class OpaqueJsonConverter<T>
+        : JsonConverter<Opaque<T>>
+        where T : notnull
+    {
+        public static OpaqueJsonConverter<T> Instance { get; } = new();
+
+        /// <inheritdoc/>
+        public override Opaque<T>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // Opaque values should never need JSON escaping
+            byte[]? buff = null;
+            ReadOnlySpan<byte> utf8Data;
+            try
+            {
+                if (reader.HasValueSequence)
+                {
+                    var seq = reader.ValueSequence;
+                    buff = ArrayPool<byte>.Shared.Rent((int)seq.Length);
+                    seq.CopyTo(buff);
+                    utf8Data = buff.AsSpan(0, (int)seq.Length);
+                } 
+                else
+                {
+                    utf8Data = reader.ValueSpan;
+                }
+
+                if (!Opaque<T>.TryParse(utf8Data, null, out var result))
+                {
+                    throw new JsonException("Failed to parse opaque value");
+                }
+
+                return result;
+            }
+            finally
+            {
+                if (buff is not null)
+                {
+                    ArrayPool<byte>.Shared.Return(buff);
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Write(Utf8JsonWriter writer, Opaque<T> value, JsonSerializerOptions options)
+        {
+            var buff = new ArrayBufferWriter<byte>();
+            {
+                using var tempWriter = new Utf8JsonWriter(buff);
+
+                // do not use options from outside - Opaque always use the same options
+                JsonSerializer.Serialize(tempWriter, value.Value, options: null);
+                tempWriter.Flush();
+            }
+
+            var span = buff.WrittenSpan;
+            var utf8Buff = ArrayPool<byte>.Shared.Rent(Base64UrlEncoder.GetMaxEncodedLength(span.Length));
+            try
+            {
+                if (!Base64UrlEncoder.TryEncode(span, utf8Buff, out var written))
+                {
+                    throw new JsonException("Failed to encod opaque value");
+                }
+
+                writer.WriteStringValue(utf8Buff.AsSpan(0, written));
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(utf8Buff);
+            }
+        }
+    }
+}
