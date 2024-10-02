935f161f508c4780857f449c792b0db24fae0721
Dhanalakshmi Gopalswamy <34273718+acn-dgopa@users.noreply.github.com>
2024-10-02 11:32:51 +0200

    Updated the type of vendor (#749)
    * Updated the type of vendor
    
    * Updated name of the organisation info
    
    ---------
    
    Co-authored-by: acn-dgopa <acn-dgopa@dev-acn-tje-14>

---
+++ src/Core/Models/VendorInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Altinn.Platform.Authentication.Core.Models
+{
+    /// summary
+    /// Organization claim matching structure from maskinporten
+    /// /summary
+    public class VendorInfo
+    {
+        /// summary
+        /// The authority that defines organization numbers. 
+        /// /summary
+        [JsonPropertyName("authority")]
+        public string Authority;
+
+        [JsonPropertyName("ID")]
+        public string ID { get; set; }
+    }
+}
