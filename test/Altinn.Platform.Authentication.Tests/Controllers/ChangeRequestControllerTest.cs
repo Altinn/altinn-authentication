11becb0d1e2c1bb61952d3eafd775ff85f9e715b
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-11-04 16:14:51 +0100

    create change request test

--- test/Altinn.Platform.Authentication.Tests/Controllers/ChangeRequestControllerTest.cs
+++ test/Altinn.Platform.Authentication.Tests/Controllers/ChangeRequestControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -34,6 +35,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Moq;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Altinn.Platform.Authentication.Tests.Controllers;
@@ -112,6 +114,142 @@ public class ChangeRequestControllerTest(
         services.AddSingleton<IChangeRequestSystemUser, ChangeRequestSystemUserService>();
         SetupDateTimeMock();
         SetupGuidMock();
+    }
+
+    /// <summary>
+    /// After having verified that the ChangeRequest is needed, create a ChangeRequest
+    /// </summary>
+    [Fact]
+    public async Task ChangeRequest_Create_ReturnOk()
+    {
+        List<XacmlJsonResult> xacmlJsonResults = GetDecisionResultSingle();
+
+        _pdpMock.Setup(p => p.GetDecisionForRequest(It.IsAny<XacmlJsonRequestRoot>())).ReturnsAsync(new XacmlJsonResponse
+        {
+            Response = xacmlJsonResults
+        });
+
+        // Create System used for test
+        string dataFileName = "Data/SystemRegister/Json/SystemRegister.json";
+        HttpResponseMessage response = await CreateSystemRegister(dataFileName);
+
+        HttpClient client = CreateClient();
+        string token = AddSystemUserRequestWriteTestTokenToClient(client);
+        string endpoint = $"/authentication/api/v1/systemuser/request/vendor";
+
+        Right right = new()
+        {
+            Resource =
+            [
+                new AttributePair()
+                {
+                    Id = "urn:altinn:resource",
+                    Value = "ske-krav-og-betalinger"
+                }
+            ]
+        };
+
+        // Arrange
+        CreateRequestSystemUser req = new()
+        {
+            ExternalRef = "external",
+            SystemId = "991825827_the_matrix",
+            PartyOrgNo = "910493353",
+            Rights = [right]
+        };
+
+        HttpRequestMessage request = new(HttpMethod.Post, endpoint)
+        {
+            Content = JsonContent.Create(req)
+        };
+        HttpResponseMessage message = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+        Assert.Equal(HttpStatusCode.Created, message.StatusCode);
+
+        RequestSystemResponse? res = await message.Content.ReadFromJsonAsync<RequestSystemResponse>();
+        Assert.NotNull(res);
+        Assert.Equal(req.ExternalRef, res.ExternalRef);
+
+        // Party Get Request
+        HttpClient client2 = CreateClient();
+        client2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", PrincipalUtil.GetToken(1337, null, 3));
+
+        int partyId = 500000;
+
+        string approveEndpoint = $"/authentication/api/v1/systemuser/request/{partyId}/{res.Id}/approve";
+        HttpRequestMessage approveRequestMessage = new(HttpMethod.Post, approveEndpoint);
+        HttpResponseMessage approveResponseMessage = await client2.SendAsync(approveRequestMessage, HttpCompletionOption.ResponseHeadersRead);
+        Assert.Equal(HttpStatusCode.OK, approveResponseMessage.StatusCode);
+
+        xacmlJsonResults = GetDecisionResultList();
+
+        _pdpMock.Setup(p => p.GetDecisionForRequest(It.IsAny<XacmlJsonRequestRoot>())).ReturnsAsync(new XacmlJsonResponse
+        {
+            Response = xacmlJsonResults
+        });
+
+        // Change Request, verify all rights permit
+        string verifyChangeRequestEndpoint = $"/authentication/api/v1/systemuser/changerequest/vendor/";
+
+        ChangeRequestSystemUser change = new()
+        {
+            ExternalRef = "external",
+            SystemId = "991825827_the_matrix",
+            PartyOrgNo = "910493353",
+            RequiredRights = [right],
+            UnwantedRights = []
+        };
+
+        HttpRequestMessage verifyChangeRequestMessage = new(HttpMethod.Post, verifyChangeRequestEndpoint)
+        {
+            Content = JsonContent.Create(change)
+        };
+        HttpResponseMessage createdResponseMessage = await client.SendAsync(verifyChangeRequestMessage, HttpCompletionOption.ResponseHeadersRead);
+        Assert.Equal(HttpStatusCode.Created, createdResponseMessage.StatusCode);
+
+        ChangeRequestResponse? createdResponse = await createdResponseMessage.Content.ReadFromJsonAsync<ChangeRequestResponse>();
+        Assert.NotNull(createdResponse);
+        Assert.NotEmpty(createdResponse.RequiredRights);
+        Assert.True(DeepCompare(createdResponse.RequiredRights, change.RequiredRights));
+    }
+
+    private static bool DeepCompare(List<Right> requiredRights1, List<Right> requiredRights2)
+    {
+        bool result = true;
+
+        if (requiredRights1.Count != requiredRights2.Count)
+        {
+            return false;
+        }               
+
+        foreach (Right right in requiredRights1) 
+        {
+            foreach (AttributePair pair in right.Resource)
+            {
+                if (!DeepFind(requiredRights2, pair.Value))
+                {                    
+                    return false;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static bool DeepFind(List<Right> requiredRights, string value)
+    {
+        foreach (Right right in requiredRights)
+        {
+            foreach (AttributePair pair in right.Resource)
+            {
+                if (pair.Value == value)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     [Fact]
@@ -298,6 +436,7 @@ public class ChangeRequestControllerTest(
         ChangeRequestResponse? verifyResponse = await verifyResponseMessage.Content.ReadFromJsonAsync<ChangeRequestResponse>();
         Assert.NotNull(verifyResponse);
         Assert.NotEmpty(verifyResponse.RequiredRights);
+        Assert.True(DeepCompare(verifyResponse.RequiredRights, change.RequiredRights));
     }
 
     private static List<XacmlJsonResult> GetDecisionResultListNotAllPermit()
