001b7d9a4ff7a4815a8df76265ba4788d29d6832
Martin Gunnerud <mgunnerud@gmail.com>
2024-12-05 12:29:51 +0100

    add test for logout and redirect after approving systemuser change request

---
+++ test/Altinn.Platform.Authentication.Tests/Mocks/ChangeRequestSystemUserServiceMock.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+using Altinn.Authorization.ProblemDetails;
+using Altinn.Platform.Authentication.Core.Models;
+using Altinn.Platform.Authentication.Core.Models.Parties;
+using Altinn.Platform.Authentication.Core.Models.SystemUsers;
+using Altinn.Platform.Authentication.Services.Interfaces;
+
+namespace Altinn.Platform.Authentication.Tests.Mocks
+{
+    /// <summary>
+    /// The service that supports the SystemUser CRUD APIcontroller
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class ChangeRequestSystemUserServiceMock : IChangeRequestSystemUser
+    {
+        /// <summary>
+        /// The Constructor
+        /// </summary>
+        public ChangeRequestSystemUserServiceMock()
+        {    
+        }
+
+        public Task<Result<bool>> ApproveAndDelegateChangeOnSystemUser(Guid requestId, int partyId, int userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ChangeRequestResponse>> CreateChangeRequest(ChangeRequestSystemUser createRequest, OrganisationNumber vendorOrgNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<bool>> DeleteChangeRequestByRequestId(Guid requestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Page<ChangeRequestResponse, Guid>>> GetAllChangeRequestsForVendor(OrganisationNumber vendorOrgNo, string systemId, Page<Guid>.Request continueRequest, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ChangeRequestResponse>> GetChangeRequestByExternalRef(ExternalRequestId externalRequestId, OrganisationNumber vendorOrgNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ChangeRequestResponse>> GetChangeRequestByGuid(Guid requestId, OrganisationNumber vendorOrgNo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ChangeRequestResponse>> GetChangeRequestByPartyAndRequestId(int party, Guid requestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<string>> GetRedirectByChangeRequestId(Guid requestId)
+        {
+            return await Task.FromResult("https://smartcloudaltinn.azurewebsites.net/changerequest");
+        }
+
+        public Task<Result<bool>> RejectChangeOnSystemUser(Guid requestId, int userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<ChangeRequestResponse>> VerifySetOfRights(ChangeRequestSystemUser validateSet, OrganisationNumber vendorOrgNo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
