1232b411a5e8d81c4cae0f4c9b4eeaad5a82e3a7
Vegard Nyeng <vegard.nyeng@gmail.com>
2024-12-11 12:35:13 +0100

    clean up testdata in case testdata is added

---
+++ test/Altinn.Platform.Authentication.SystemIntegrationTests/Domain/SystemDto.cs
@@ -0,0 +1,14 @@
+namespace Altinn.Platform.Authentication.SystemIntegrationTests.Domain;
+
+// DTO Classes
+public class SystemDto
+{
+    public string SystemId { get; set; }
+    public string SystemVendorOrgNumber { get; set; }
+    public string SystemVendorOrgName { get; set; }
+}
+
+public class SystemsDto
+{
+    public List<SystemDto> Systems { get; set; }
+}
