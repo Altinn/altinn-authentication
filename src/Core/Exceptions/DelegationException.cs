e4e85694bcae5ecdb28a678b2c547fab9b93eb7c
Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>
2025-01-29 11:45:36 +0100

    ignore code coverage for delgationexception

--- src/Core/Exceptions/DelegationException.cs
+++ src/Core/Exceptions/DelegationException.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Altinn.Platform.Authentication.Core.Exceptions
 {
+    [ExcludeFromCodeCoverage]
     public class DelegationException : Exception
     {
         public ProblemDetails ProblemDetails { get; }
