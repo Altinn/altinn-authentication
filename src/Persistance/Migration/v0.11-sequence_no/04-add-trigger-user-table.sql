4bca9c38c894195208b05d82b8e72f744a02fe84
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2025-02-03 12:10:21 +0100

    add trigger on update

---
+++ src/Persistance/Migration/v0.11-sequence_no/04-add-trigger-user-table.sql
@@ -0,0 +1,14 @@
+-- ALTER TABLE : business_application.system_user_profile
+
+CREATE OR REPLACE FUNCTION business_application.update_sequence_no()
+RETURNS TRIGGER AS $BODY$
+BEGIN
+  NEW.version_id = register.tx_nextval('business_application.sequence_no');
+  RETURN NEW;
+END
+$BODY$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER update_systemuser_seqno
+BEFORE UPDATE on business_application.system_user_profile
+FOR EACH ROW EXECUTE PROCEDURE business_application.update_sequence_no();
