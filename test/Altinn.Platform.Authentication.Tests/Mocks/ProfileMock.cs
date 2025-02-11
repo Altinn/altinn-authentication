82eca3bed0ef66b7b2da1f8c38433d11f56dcc3a
Rune T. Larsen <rune@hjemmekino.no>
2025-02-11 14:46:15 +0100

    Added mock

---
+++ test/Altinn.Platform.Authentication.Tests/Mocks/ProfileMock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Altinn.Platform.Authentication.Model;
+using Altinn.Platform.Authentication.Services.Interfaces;
+using Altinn.Platform.Profile.Models;
+
+namespace Altinn.Platform.Authentication.Tests.Mocks
+{
+    public class ProfileMock : IProfile
+    {
+        public Task<UserProfile> GetUserProfile(UserProfileLookup profileLookup)
+        {
+           UserProfile userProfile = new UserProfile();
+           userProfile.UserId = profileLookup.UserId;
+           userProfile.UserUuid = Guid.NewGuid();
+           return Task.FromResult(userProfile);
+        }
+    }
+}
