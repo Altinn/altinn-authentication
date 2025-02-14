257647ca78a4c6d5ff29de9e0b3aadd47ea144ca
Vegard Wright <vegard.nyeng@gmail.com>
2025-02-14 13:29:44 +0100

    Created test for /PUT Systemuser and reported bug for it #1074 (#1075)
    * Created test for /PUT Systemuser and reported bug for it #1074 (added skip step for now)

--- test/Altinn.Platform.Authentication.SystemIntegrationTests/Utils/UrlConstants.cs
+++ test/Altinn.Platform.Authentication.SystemIntegrationTests/Utils/UrlConstants.cs
@@ -17,6 +17,8 @@ namespace Altinn.Platform.Authentication.SystemIntegrationTests.Utils
         public const string DeleteRequest = "v1/systemuser/request/vendor/{requestId}";
         public const string GetSystemUserForParty = "v1/systemuser/{party}";
         public const string GetByExternalRef = "v1/systemuser/request/vendor/byexternalref/{systemId}/{orgNo}/{externalRef}";
+        public const string PutSystemUserUrlTemplate = "v1/systemuser/";
+
         
         //API Endpoint Maskinporten
         public const string GetByExternalId = "v1/systemuser/byExternalId";
