5726e2cb9ca755d55bca2128f2efae12cb930071
Dhanalakshmi Gopalswamy <34273718+acn-dgopa@users.noreply.github.com>
2025-02-24 12:38:59 +0100

    Enable accesspackage for system (#1077)
    * Enable accesspackage for system
    
    * Added update endpoint and tests for the same
    
    * Update access package model and updated tests
    
    * Added get endpoint for access packages
    
    * comment out accesspackage metadata validation
    
    * refactored createrefisteredsystem to fix sonarcloud warning
    
    * Refactored validations and fixed sonar cloud errors
    
    * Fix more sonarcloud warnings
    
    ---------
    
    Co-authored-by: Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>

---
+++ src/Authentication/Constants/ErrorPathConstant.cs
@@ -0,0 +1,38 @@
+﻿namespace Altinn.Platform.Authentication.Constants
+{
+    /// <summary>
+    /// Constants related to error paths
+    /// </summary>
+    public static class ErrorPathConstant
+    {
+        /// <summary>
+        /// Error path for vendor id
+        /// </summary>
+        public const string VENDOR_ID = "/registersystemrequest/vendor/id";
+
+        /// <summary>
+        /// Error path for systemid
+        /// </summary>
+        public const string SYSTEM_ID = "/registersystemrequest/systemid";
+
+        /// <summary>
+        /// Error path for allowedredirecturls
+        /// </summary>
+        public const string ALLOWEDREDIRECT_URLS = "/registersystemrequest/allowedredirecturls";
+
+        /// <summary>
+        /// Error path for clientid
+        /// </summary>
+        public const string CLIENT_ID = "/registersystemrequest/clientid";
+
+        /// <summary>
+        /// Error path for accesspackages
+        /// </summary>
+        public const string ACCESSPACKAGES = "/registersystemrequest/accesspackages";
+
+        /// <summary>
+        /// Error path for resource
+        /// </summary>
+        public const string RESOURCERIGHTS = "/registersystemrequest/rights/resource";
+    }
+}
---
+++ src/Persistance/Constants/SystemRegisterFieldConstants.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altinn.Platform.Authentication.Persistance.Constants
+{
+    /// <summary>
+    /// Constants related to system register field in systemregister tabell
+    /// </summary>
+    public static class SystemRegisterFieldConstants
+    {
+        /// <summary>
+        /// Field Name for system id
+        /// </summary>
+        public const string SYSTEM_ID = "system_id";
+
+        /// <summary>
+        /// Field name for system vendor org number
+        /// </summary>
+        public const string SYSTEM_VENDOR_ORGNUMBER = "systemvendor_orgnumber";
+
+        /// <summary>
+        /// Field Name for system name
+        /// </summary>
+        public const string SYSTEM_NAME = "name";
+
+        /// <summary>
+        /// Field Name for system description
+        /// </summary>
+        public const string SYSTEM_DESCRIPTION = "description";
+
+        /// <summary>
+        /// Field Name for system description
+        /// </summary>
+        public const string SYSTEM_CLIENTID = "client_id";
+
+        /// <summary>
+        /// Field Name for system isvisible
+        /// </summary>
+        public const string SYSTEM_IS_VISIBLE = "is_visible";
+
+        /// <summary>
+        /// Field Name for allowed syste redirect urls
+        /// </summary>
+        public const string SYSTEM_ALLOWED_REDIRECTURLS = "allowedredirecturls";
+
+        /// <summary>
+        /// Field Name for system rights
+        /// </summary>
+        public const string SYSTEM_RIGHTS = "rights";
+
+        /// <summary>
+        /// Field Name for access packages
+        /// </summary>
+        public const string SYSTEM_ACCESSPACKAGES = "accesspackages";
+
+        /// <summary>
+        /// Field Name for internal system id
+        /// </summary>
+        public const string SYSTEM_INTERNAL_ID = "system_internal_id";
+
+        /// <summary>
+        /// Field Name for is deleted
+        /// </summary>
+        public const string SYSTEM_IS_DELETED = "is_deleted";
+    }
+}
