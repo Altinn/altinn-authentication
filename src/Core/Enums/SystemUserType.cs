430e6493951eb2803e4cb7cc95e0734c9207bc7d
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2025-03-07 08:36:01 +0100

    merge commit

--- src/Core/Enums/SystemUserType.cs
+++ src/Core/Enums/SystemUserType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Altinn.Platform.Authentication.Core.Enums
+﻿namespace Altinn.Platform.Authentication.Core.Enums
 {
     public enum SystemUserType
     {
