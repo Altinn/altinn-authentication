88196db5cfa2722b443d62a03cb06460023e2d01
Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>
2025-03-10 11:09:18 +0100

    Fix redirecturl validation

--- test/Altinn.Platform.Authentication.Tests/AuthenticationHelperTests.cs
+++ test/Altinn.Platform.Authentication.Tests/AuthenticationHelperTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Altinn.Authentication.Core.Problems;
 using Altinn.Platform.Authentication.Core.Models;
+using Altinn.Platform.Authentication.Core.SystemRegister.Models;
 using Altinn.Platform.Authentication.Helpers;
 using Xunit;
 
@@ -12,24 +15,24 @@ namespace Altinn.Platform.Authentication.Tests
         {
             // Arrange
             var rights = new List<Right>
-            {
-                new Right
                 {
-                    Action = "Read",
-                    Resource = new List<AttributePair>
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
-                    }
-                },
-                new Right
-                {
-                    Action = "Write",
-                    Resource = new List<AttributePair>
+                        Action = "Read",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        }
+                    },
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value2" }
+                        Action = "Write",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value2" }
+                        }
                     }
-                }
-            };
+                };
 
             // Act
             bool result = AuthenticationHelper.HasDuplicateRights(rights);
@@ -43,24 +46,24 @@ namespace Altinn.Platform.Authentication.Tests
         {
             // Arrange
             var rights = new List<Right>
-            {
-                new Right
                 {
-                    Action = "Read",
-                    Resource = new List<AttributePair>
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
-                    }
-                },
-                new Right
-                {
-                    Action = "Write",
-                    Resource = new List<AttributePair>
+                        Action = "Read",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        }
+                    },
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        Action = "Write",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        }
                     }
-                }
-            };
+                };
 
             // Act
             bool result = AuthenticationHelper.HasDuplicateRights(rights);
@@ -74,24 +77,24 @@ namespace Altinn.Platform.Authentication.Tests
         {
             // Arrange
             var rights = new List<Right>
-            {
-                new Right
                 {
-                    Action = "Read",
-                    Resource = new List<AttributePair>
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
-                    }
-                },
-                new Right
-                {
-                    Action = "Read",
-                    Resource = new List<AttributePair>
+                        Action = "Read",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        }
+                    },
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        Action = "Read",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        }
                     }
-                }
-            };
+                };
 
             // Act
             bool result = AuthenticationHelper.HasDuplicateRights(rights);
@@ -118,22 +121,117 @@ namespace Altinn.Platform.Authentication.Tests
         {
             // Arrange
             var rights = new List<Right>
-            {
-                new Right
                 {
-                    Action = "Read",
-                    Resource = new List<AttributePair>
+                    new Right
                     {
-                        new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        Action = "Read",
+                        Resource = new List<AttributePair>
+                        {
+                            new AttributePair { Id = "urn:altinn:resource", Value = "value1" }
+                        }
                     }
-                }
-            };
+                };
 
             // Act
             bool result = AuthenticationHelper.HasDuplicateRights(rights);
 
             // Assert
             Assert.False(result);
+        }
+
+        [Fact]
+        public void ValidateRedirectUrl_ValidUrlWithoutQuery_ReturnsTrue()
+        {
+            // Arrange
+            var systemInfo = new RegisteredSystem
+            {
+                Id = "system1",
+                Vendor = new VendorInfo { ID = "Vendor1" },
+                Name = new Dictionary<string, string> { { "en", "System 1" } },
+                Description = new Dictionary<string, string> { { "en", "Description of System 1" } },
+                AllowedRedirectUrls = new List<Uri>
+                    {
+                        new Uri("https://example.com/callback")
+                    }
+            };
+            string redirectURL = "https://example.com/callback";
+
+            // Act
+            var result = AuthenticationHelper.ValidateRedirectUrl(redirectURL, systemInfo);
+
+            // Assert
+            Assert.True(result.Value);
+        }
+
+        [Fact]
+        public void ValidateRedirectUrl_ValidUrlWithQuery_ReturnsTrue()
+        {
+            // Arrange
+            var systemInfo = new RegisteredSystem
+            {
+                Id = "system1",
+                Vendor = new VendorInfo { ID = "Vendor1" },
+                Name = new Dictionary<string, string> { { "en", "System 1" } },
+                Description = new Dictionary<string, string> { { "en", "Description of System 1" } },
+                AllowedRedirectUrls = new List<Uri>
+                    {
+                        new Uri("https://example.com/callback")
+                    }
+            };
+            string redirectURL = "https://example.com/callback?param=value";
+
+            // Act
+            var result = AuthenticationHelper.ValidateRedirectUrl(redirectURL, systemInfo);
+
+            // Assert
+            Assert.True(result.Value);
+        }
+
+        [Fact]
+        public void ValidateRedirectUrl_InvalidUrl_ReturnsProblem()
+        {
+            // Arrange
+            var systemInfo = new RegisteredSystem
+            {
+                Id = "system1",
+                Vendor = new VendorInfo { ID = "Vendor1" },
+                Name = new Dictionary<string, string> { { "en", "System 1" } },
+                Description = new Dictionary<string, string> { { "en", "Description of System 1" } },
+                AllowedRedirectUrls = new List<Uri>
+                    {
+                        new Uri("https://example.com/callback")
+                    }
+            };
+            string redirectURL = "https://invalid.com/callback";
+
+            // Act
+            var result = AuthenticationHelper.ValidateRedirectUrl(redirectURL, systemInfo);
+
+            // Assert
+            Assert.False(result.Value);
+            Assert.Equal(Problem.RedirectUriNotFound, result.Problem);
+        }
+
+        [Fact]
+        public void ValidateRedirectUrl_EmptyAllowedRedirectUrls_ReturnsProblem()
+        {
+            // Arrange
+            var systemInfo = new RegisteredSystem
+            {
+                Id = "system1",
+                Vendor = new VendorInfo { ID = "Vendor1" },
+                Name = new Dictionary<string, string> { { "en", "System 1" } },
+                Description = new Dictionary<string, string> { { "en", "Description of System 1" } },
+                AllowedRedirectUrls = new List<Uri>()
+            };
+            string redirectURL = "https://example.com/callback";
+
+            // Act
+            var result = AuthenticationHelper.ValidateRedirectUrl(redirectURL, systemInfo);
+
+            // Assert
+            Assert.False(result.Value);
+            Assert.Equal(Problem.RedirectUriNotFound, result.Problem);
         }
     }
 }
