39b943389d0cd9b0b91a2d56d57e803d1b0d2af8
Dagfinn Olsen <dagfinno@gmail.com>
2025-03-17 08:01:41 +0100

    added get system by external id ++

--- test/Altinn.Platform.Authentication.PerformanceTests/common/config.js
+++ test/Altinn.Platform.Authentication.PerformanceTests/common/config.js
@@ -3,6 +3,9 @@ const yt01BaseUrl = "https://platform.yt01.altinn.cloud/authentication/";
 const systemRegister = "api/v1/systemregister/vendor";
 const systemUserRequest = "api/v1/systemuser/request/vendor";
 const systemUserApprove = "api/v1/systemuser/request/";
+const systemUrl = "api/v1/systemregister"
+const systemUsersUrl = "api/v1/systemuser/vendor/bysystem/"
+const systemUserByExternalIdUrl = "api/v1/systemuser/byExternalId"
 export const urls = {
     v1: {
         registerSystem: {
@@ -18,6 +21,18 @@ export const urls = {
         approveSystemUser: {
             test: testBaseUrl + systemUserApprove,
             yt01: yt01BaseUrl + systemUserApprove
+        },
+        systemUrl: {
+            test: testBaseUrl + systemUrl,
+            yt01: yt01BaseUrl + systemUrl
+        },
+        systemUsersUrl: {
+            test: testBaseUrl + systemUsersUrl,
+            yt01: yt01BaseUrl + systemUsersUrl
+        },
+        systemUserByExternalIdUrl: {
+            test: testBaseUrl + systemUserByExternalIdUrl,
+            yt01: yt01BaseUrl + systemUserByExternalIdUrl
         }
     }
 };
@@ -33,4 +48,7 @@ if (!urls[__ENV.API_VERSION]["registerSystem"][__ENV.API_ENVIRONMENT]) {
 export const registerSystemUrl = urls[__ENV.API_VERSION]["registerSystem"][__ENV.API_ENVIRONMENT];
 export const requestSystemUserUrl = urls[__ENV.API_VERSION]["requestSystemUser"][__ENV.API_ENVIRONMENT];
 export const approveSystemUserUrl = urls[__ENV.API_VERSION]["approveSystemUser"][__ENV.API_ENVIRONMENT];
+export const getSystemUrl = urls[__ENV.API_VERSION]["systemUrl"][__ENV.API_ENVIRONMENT];
+export const getSystemUsersUrl = urls[__ENV.API_VERSION]["systemUsersUrl"][__ENV.API_ENVIRONMENT];
+export const getSystemUserByExternalIdUrl = urls[__ENV.API_VERSION]["systemUserByExternalIdUrl"][__ENV.API_ENVIRONMENT];
 export const tokenGeneratorEnv = __ENV.API_ENVIRONMENT == "yt01" ? "yt01" : "tt02"; // yt01 is the only environment that has a separate token generator environment
