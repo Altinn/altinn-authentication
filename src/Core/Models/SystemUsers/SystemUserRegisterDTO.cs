1f755d82ebac0f7280f71e9576fd6dab1389e64e
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2025-09-10 10:54:36 +0200

    Systemuser stream to register (#1433)
    * fromBff fix
    
    * added partyId and enum systemusertype

--- src/Core/Models/SystemUsers/SystemUserRegisterDTO.cs
+++ src/Core/Models/SystemUsers/SystemUserRegisterDTO.cs
@@ -1,55 +1,69 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using Altinn.Platform.Authentication.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
-namespace Altinn.Platform.Authentication.Core.Models.SystemUsers
+namespace Altinn.Platform.Authentication.Core.Models.SystemUsers;
+
+/// <summary>
+/// DTO for syncing the SystemUsers to the Register 
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class SystemUserRegisterDTO
 {
     /// <summary>
-    /// DTO for syncing the SystemUsers to the Register 
+    /// GUID Id for the SystemUser
     /// </summary>
-    [ExcludeFromCodeCoverage]
-    public class SystemUserRegisterDTO
-    {
-        /// <summary>
-        /// GUID Id for the SystemUser
-        /// </summary>
-        [JsonPropertyName("id")]
-        public required string Id { get; set; }
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
 
-        /// <summary>
-        /// The Title is by default the same as the System's Display Name
-        /// </summary>
-        [JsonPropertyName("integrationTitle")]
-        public required string IntegrationTitle { get; set; }
+    /// <summary>
+    /// The organisation number for the SystemUser's Party ( the customer that delegates rights to the systemuser).
+    /// Is one of the three parts of the External Request Id.
+    /// </summary>
+    [JsonPropertyName("partyOrgNo")]
+    public required string PartyOrgNo { get; set; }
 
-        /// <summary>
-        /// False by default, if a SystemUser is deleted in the API,
-        /// it is marked as IsDeleted ("tombstoned") rather than actually deleted.
-        /// </summary>
-        [JsonPropertyName("isDeleted")]
-        public bool IsDeleted { get; set; } = false;
+    /// <summary>
+    /// The Reportee Id, used internally, old integer format, exchanged in the DTO as string
+    /// </summary>
+    [JsonPropertyName("partyId")]
+    public required string PartyId { get; set; }
 
-        /// <summary>
-        /// Last changed data time
-        /// </summary>
-        [JsonPropertyName("lastChanged")]
-        public DateTime LastChanged {  get; set; }
+    /// <summary>
+    /// The Title is by default the same as the System's Display Name
+    /// </summary>
+    [JsonPropertyName("integrationTitle")]
+    public required string IntegrationTitle { get; set; }
 
-        /// <summary>
-        /// Created date time
-        /// </summary>
-        [JsonPropertyName("created")]
-        public DateTime Created { get; set; } 
+    /// <summary>
+    /// False by default, if a SystemUser is deleted in the API,
+    /// it is marked as IsDeleted ("tombstoned") rather than actually deleted.
+    /// </summary>
+    [JsonPropertyName("isDeleted")]
+    public bool IsDeleted { get; set; } = false;
 
-        /// <summary>
-        /// Used internally when paginating
-        /// </summary>
-        [JsonPropertyName("sequenceNo")]
-        public long SequenceNo { get; set; } = 0;
+    /// <summary>
+    /// Last changed data time
+    /// </summary>
+    [JsonPropertyName("lastChanged")]
+    public DateTime LastChanged {  get; set; }
 
-        /// <summary>
-        /// Either Agent or Default
-        /// </summary>
-        [JsonPropertyName("systemUserType")]
-        public string SystemUserType { get; set; }
-    }
+    /// <summary>
+    /// Created date time
+    /// </summary>
+    [JsonPropertyName("created")]
+    public DateTime Created { get; set; } 
+
+    /// <summary>
+    /// Used internally when paginating
+    /// </summary>
+    [JsonPropertyName("sequenceNo")]
+    public long SequenceNo { get; set; } = 0;
+
+    /// <summary>
+    /// Either Agent or Default
+    /// </summary>
+    [JsonPropertyName("systemUserType")]
+    public SystemUserType SystemUserType { get; set; }
 }
