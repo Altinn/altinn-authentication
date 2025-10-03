b2d0d4f23b43bd18cc59fc5cf82eb9a8a33c8eef
Rune T. Larsen <rune.tommeras.larsen@accenture.com>
2025-10-03 08:48:14 +0200

    local commit

--- test/Altinn.Platform.Authentication.Tests/Helpers/TokenAssertsHelper.cs
+++ test/Altinn.Platform.Authentication.Tests/Helpers/TokenAssertsHelper.cs
@@ -15,6 +15,7 @@ namespace Altinn.Platform.Authentication.Tests.Helpers
             Assert.Equal("Bearer", tokenResponseDto.token_type);
             Assert.True(tokenResponseDto.expires_in > 0);
             Assert.NotNull(tokenResponseDto.id_token); // since openid scope was requested
+            Assert.NotNull(tokenResponseDto.refresh_token);
             Assert.Contains("openid", tokenResponseDto.scope.Split(' '));
             Assert.Contains("altinn:portal/enduser", tokenResponseDto.scope.Split(' '));
             AssertAccessToken(tokenResponseDto.access_token, testScenario);
