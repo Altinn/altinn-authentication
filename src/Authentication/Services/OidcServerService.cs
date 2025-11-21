145dde932746546f2e86dab243c2ae9d48c6dfda
Dhanalakshmi Gopalswamy <acn-dgopa@ai-dev.no>
2025-11-21 13:19:30 +0100

    Update upstreamsessionid to externalserssionid

--- src/Authentication/Services/OidcServerService.cs
+++ src/Authentication/Services/OidcServerService.cs
@@ -1181,7 +1181,7 @@ namespace Altinn.Platform.Authentication.Services
                     Amr = userIdenity.Amr,
                     Scopes = scopes,
                     ExpiresAt = _timeProvider.GetUtcNow().AddMinutes(_generalSettings.JwtValidityMinutes),
-                    UpstreamSessionSid = userIdenity.Sid,
+                    UpstreamSessionSid = userIdenity.ExternalSessionId,
                     Now = _timeProvider.GetUtcNow(),
                     CreatedByIp = authInput.CreatedByIp,
                     UserAgentHash = authInput.UserAgentHash,
