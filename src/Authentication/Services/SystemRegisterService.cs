5162683e5abb08d8d93fc9ba80ddab4521b28ed1
Vegard Nyeng <vegard.nyeng@gmail.com>
2025-11-26 14:37:13 +0100

    adjusted whitelisted resource types

--- src/Authentication/Services/SystemRegisterService.cs
+++ src/Authentication/Services/SystemRegisterService.cs
@@ -30,8 +30,6 @@ namespace Altinn.Platform.Authentication.Services
         private static readonly HashSet<ResourceType> WhitelistedResourceTypes = new()
         {
             ResourceType.AltinnApp,
-            ResourceType.Systemresource,
-            ResourceType.Default,
             ResourceType.CorrespondenceService,
             ResourceType.BrokerService,
             ResourceType.GenericAccessResource,           
