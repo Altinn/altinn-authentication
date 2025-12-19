b5e7e3adf69ade81bcbb23dbf8062a83b2d5167d
Martin Gunnerud <mgunnerud@gmail.com>
2025-12-19 15:45:51 +0100

    serialize Created field for system user request models (#1758)

--- src/Core/Models/SystemUsers/ChangeRequestResponse.cs
+++ src/Core/Models/SystemUsers/ChangeRequestResponse.cs
@@ -110,7 +110,6 @@ public class ChangeRequestResponse()
     /// The date and time the Request was created,
     /// used to determine if the Request is still valid.
     /// </summary>
-    [JsonIgnore]
     public DateTime Created { get; set; }
 
     /// <summary>
