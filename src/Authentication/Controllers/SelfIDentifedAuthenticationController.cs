6cd1d8505cfb52079a4c219d124a8e9720aee9b7
Rune T. Larsen <rune.tommeras-larsen@digdir.no>
2026-05-05 18:42:12 +0200

    Update

--- src/Authentication/Controllers/SelfIDentifedAuthenticationController.cs
+++ src/Authentication/Controllers/SelfIDentifedAuthenticationController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Threading.Tasks;
+using Altinn.Platform.Authentication.Core.Constants;
 using Altinn.Platform.Authentication.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Altinn.Platform.Authentication.Controllers
 {
     /// <summary>
     /// Controller for self identified authentication. This is used for users that are not registered in the system, but still need to be able to authenticate and use the system. The controller should have methods that validates the credentials of the user and returns a token that can be used for subsequent requests.
-    /// 
+    ///
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
+    [HttpPost("validate-credentials")]
+    [Authorize(Policy = AuthzConstants.POLICY_SCOPE_PORTAL)]
     public class SelfIDentifedAuthenticationController(IUserProfileService profileService) : ControllerBase
     {
         private readonly IUserProfileService _profileService = profileService;
